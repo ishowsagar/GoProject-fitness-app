6a5da2cab96d7098d4a7885afb026674712e9081
Sagar <ishowsagar777@gmail.com>
2026-02-12 12:05:46 +0530

    Add migration to alter workouts table for user_id reference

---
+++ migrations/00005_user_id_alter.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE workouts
+ADD COLUMN IF NOT EXISTS user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE workouts DROP COLUMN IF EXISTS user_id; 
+-- +goose StatementEnd
